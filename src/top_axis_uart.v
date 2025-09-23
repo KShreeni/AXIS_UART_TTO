912ffc76e1e85afbb390a0ad69672a07194daf2b
KShreeni <109845020+KShreeni@users.noreply.github.com>
2025-09-23 13:50:26 +0530

    Update top_axis_uart.v

--- src/top_axis_uart.v
+++ src/top_axis_uart.v
@@ -22,15 +22,19 @@
 
 module top_axis_uart #(parameter DATA_BITS = 8) (input clk,rst,input wire [7:0] axis_data,
 input wire axis_valid,
+input wire [3:0] index,
 input wire axis_last,output wire uart_tx,rx_valid,output wire [DATA_BITS-1:0]rx_data);
+
+wire [7:0] m_axis_data;
 wire axis_ready;
 
+wire m_axis_valid_out;
 
-axis_master_inp #(.WIDTH(8)) mast_inst(.clk(clk),.rst(rst),
-.m_axis_valid(axis_valid),.m_axis_ready(axis_ready),.m_axis_last(axis_last),.m_axis_data(axis_data));
+axis_master_inp #(.WIDTH(8)) mast_inst(.clk(clk),.rst(rst),.load_data(axis_data),.load_index(index),
+.m_axis_valid(axis_valid),.m_axis_ready(axis_ready),.m_axis_last(axis_last),.m_axis_valid_out(m_axis_valid_out),.m_axis_data(m_axis_data));
 
 axis_fifo_uart_tx #(.WIDTH(8),.DEPTH(8),.CLK_RATE(100000000),.BAUD(115200)) axis_fifo_uart_tx_inst(.clk(clk),.rst(rst),
-.s_axis_data(axis_data),.s_axis_valid(axis_valid),.s_axis_ready(axis_ready),.s_axis_last(axis_last),.uart_tx(uart_tx));
+.s_axis_data(m_axis_data),.s_axis_valid(m_axis_valid_out),.s_axis_ready(axis_ready),.s_axis_last(axis_last),.uart_tx(uart_tx));
 
 uart_rec #(.CLK_FREQ(100000000),.BAUD(115200),.DATA_BITS(DATA_BITS)) 
 uart_rec_inst(.clk(clk),.rst(rst),.rx(uart_tx),.rx_data(rx_data),.rx_valid(rx_valid));
