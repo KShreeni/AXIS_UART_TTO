1f14b2f708c39eb95890b52ce1196f279ebef099
KShreeni <109845020+KShreeni@users.noreply.github.com>
2025-09-23 15:02:05 +0530

    Update uart_rec.v

--- src/uart_rec.v
+++ src/uart_rec.v
@@ -32,14 +32,14 @@ module uart_rec #(
     output reg  rx_valid         // High for 1 clk when new data is ready
 );
 
-    localparam [10:0] BAUD_DIV = (CLK_FREQ/BAUD);
+    localparam  BAUD_DIV = (CLK_FREQ/BAUD);
    
 
     localparam IDLE  = 2'd0,
                START = 2'd1,
                DATA  = 2'd2,
                STOP  = 2'd3;
-
+    localparam Baud_cnt_width = $clog2(BAUD_DIV);
     reg [1:0] state, next_state;
     reg [$clog2(BAUD_DIV):0] baud_cnt;
     reg [$clog2(DATA_BITS):0] bit_cnt;
@@ -66,14 +66,14 @@ module uart_rec #(
                     end
                 end
                 START: begin
-                    if (baud_cnt == BAUD_DIV/2) begin
+                    if (baud_cnt == BAUD_DIV[Baud_cnt_width-1:0]/2) begin
                         baud_cnt <= 0;
                         bit_cnt <= 0;
                         next_state <= DATA;
                     end else baud_cnt <= baud_cnt + 1;
                 end
                 DATA: begin
-                    if (baud_cnt == BAUD_DIV-1) begin
+                    if (baud_cnt == (BAUD_DIV[Baud_cnt_width-1:0] - 1)) begin
                         baud_cnt <= 0;
                         shift_reg <= {rx, shift_reg[DATA_BITS-1:1]}; // LSB first
                         if (bit_cnt == DATA_BITS-1) begin
@@ -82,7 +82,7 @@ module uart_rec #(
                     end else baud_cnt <= baud_cnt + 1;
                 end
                 STOP: begin
-                    if (baud_cnt == BAUD_DIV-1) begin
+                    if (baud_cnt ==  (BAUD_DIV[Baud_cnt_width-1:0] - 1)) begin
                         baud_cnt <= 0;
                         rx_data <= shift_reg;
                         rx_valid <= 1;
