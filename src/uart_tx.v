0fb72720cb413ed36501741ef0ba4f1be3ee284c
KShreeni <109845020+KShreeni@users.noreply.github.com>
2025-10-08 12:32:24 +0530

    Update uart_tx.v

--- src/uart_tx.v
+++ src/uart_tx.v
@@ -20,139 +20,131 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module uart_tx #(parameter clk_rate = 100000000, parameter Baud = 115200, parameter Word_len = 8
+module uart_tx #(
+    parameter clk_rate = 50_000_000,
+    parameter Baud     = 115200,
+    parameter Word_len = 8,
+    parameter PARITY   = "even" // Options: "none", "even", "odd"
 )
 (
-input clk,rst,
-input [Word_len-1:0]tx_data,
-input tx_data_valid,tx_data_last,
-output wire tx_data_ready,
-output reg Uart_tx
+    input clk, rst,
+    input [Word_len-1:0] tx_data,
+    input tx_data_valid,tx_data_last,
+    output wire tx_data_ready,
+    output reg Uart_tx
 );
 
-  localparam  Baud_div = (clk_rate/Baud);
-localparam  Baud_cnt_width = $clog2(Baud_div);
-//localparam NORM_WAIT = Baud_div - 1;
-//localparam PACKET_WAIT = (Baud_div * 2) - 1;
+    localparam Baud_div = (clk_rate / Baud);
+    
+    // FSM States
+    localparam Idle   = 3'd0,
+               Start  = 3'd1,
+               Data   = 3'd2,
+               Parity = 3'd3,
+               Stop   = 3'd4;
 
-localparam Idle = 2'd0, Start = 2'd1, Data = 2'd2, Stop = 2'd3;
+    reg [2:0] current_state, next_state;
 
-reg[1:0] current_state,next_state; 
+    reg [$clog2(Baud_div)-1:0] baud_cnt;
+    reg [$clog2(Word_len)-1:0] bit_cnt;
+    reg [Word_len-1:0] shift_reg;
+    reg parity_bit;
 
-  reg[Baud_cnt_width:0] baud_cnt;
-reg[$clog2(Word_len+1)-1:0] bit_cnt;
-reg[Word_len-1:0] shift_reg;
+    assign tx_data_ready = (current_state == Idle);
 
-assign tx_data_ready = (current_state == Idle);  
+    always @(posedge clk or posedge rst) begin
+        if (rst)
+            current_state <= Idle;
+        else
+            current_state <= next_state;
+    end
 
-always@(posedge clk or posedge rst)begin
-  if(rst)begin
-  current_state <= Idle;
-  end
-  else begin
-     current_state <= next_state;
-  end
-end
-
-always@(*)begin
-next_state = current_state;
-case(current_state)
-   Idle : begin
-         if(tx_data_valid) next_state = Start;
-         else next_state = Idle;
-   end
-   
-   Start : begin
-          if(baud_cnt ==  (Baud_div[Baud_cnt_width-1:0] - 1)) next_state = Data;
-          else next_state = Start;
-   end
-   
-   Data : begin
-          if(bit_cnt == Word_len-1 && baud_cnt == (Baud_div[Baud_cnt_width-1:0] - 1)) next_state = Stop;
-          else next_state = Data;
-   end
-   
-   Stop : begin
-     if(baud_cnt == (Baud_div[Baud_cnt_width-1:0] - 1)&& tx_data_last) next_state = Idle;
-          else next_state = Stop;
-   end
-   
-   /*Wait : begin
-          if(baud_cnt == (tx_data_last? PACKET_WAIT:NORM_WAIT)) 
-             next_state = Idle;
-          else next_state = Wait;
-   end*/
-   
-   default : next_state = Idle;
-   
-   endcase
-end
-
-always@(posedge clk or posedge rst)begin
- if(rst)begin
-   baud_cnt <= 0;
-   bit_cnt <= 0;
-   shift_reg <= {Word_len{1'b0}};
-   Uart_tx <= 1'b1;
- end
- else begin
-  
-   case(current_state)
-     Idle : begin
-           baud_cnt <= 0;
-           bit_cnt <= 0;
-           Uart_tx <= 1'b1;
-            if(tx_data_valid && tx_data_ready) shift_reg <= tx_data;  
-     end
-     
-     Start : begin
-            Uart_tx <= 1'b0;
-            if(baud_cnt == (Baud_div[Baud_cnt_width-1:0] - 1))begin
-              baud_cnt <= 0;
+    always @(*) begin
+        next_state = current_state;
+        case (current_state)
+            Idle: begin
+                if (tx_data_valid) next_state = Start;
             end
-            else begin
-              baud_cnt <= baud_cnt+ 1;
+            Start: begin
+                if (baud_cnt == (Baud_div - 1)) next_state = Data;
             end
-     end
-     
-     Data : begin
-           Uart_tx <= shift_reg[0];
-           if(baud_cnt == (Baud_div[Baud_cnt_width-1:0] - 1))begin
-             baud_cnt <= 0;
-             shift_reg <= {1'b0,shift_reg[Word_len-1:1]};
-             if(bit_cnt == Word_len-1)
-               bit_cnt <= 0;
-             else
-               bit_cnt <= bit_cnt + 1;
-           end
-           else 
-             baud_cnt <= baud_cnt + 1;
-     end
-     
-     Stop : begin
-           Uart_tx <= 1'b1;
-           if(baud_cnt == (Baud_div[Baud_cnt_width-1:0] - 1))
-              baud_cnt <= 0;
-           else
-              baud_cnt <= baud_cnt + 1;
-     end
-     
-   /*  Wait : begin
-           Uart_tx <= 1'b1;
-           
-           if(baud_cnt == (tx_data_last? PACKET_WAIT:NORM_WAIT))
-              baud_cnt <= 0;
-           else
-              baud_cnt <= baud_cnt + 1;
-     end*/
-     
-     default : begin
-               baud_cnt <= 0;
-               bit_cnt <= 0;
-               shift_reg <= {Word_len{1'b0}};
-               Uart_tx <= 1'b1;
-     end
-   endcase
- end
-end
+            Data: begin
+                if (bit_cnt == Word_len - 1 && baud_cnt == (Baud_div - 1)) begin
+                    if (PARITY == "none")
+                        next_state = Stop;
+                    else
+                        next_state = Parity;
+                end
+            end
+            Parity: begin
+                if (baud_cnt == (Baud_div - 1)) next_state = Stop;
+            end
+            Stop: begin
+                if(baud_cnt == (Baud_div - 1)) next_state = Idle;
+                 else if (tx_data_last)next_state = Idle;
+                  else next_state = Stop;
+            end
+            default: next_state = Idle;
+        endcase
+    end
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            baud_cnt <= 0;
+            bit_cnt <= 0;
+            shift_reg <= 0;
+            Uart_tx <= 1'b1;
+            parity_bit <= 1'b0;
+        end else begin
+            case (current_state)
+                Idle: begin
+                    baud_cnt <= 0;
+                    bit_cnt <= 0;
+                    Uart_tx <= 1'b1;
+                    if (tx_data_valid && tx_data_ready) begin
+                        shift_reg <= tx_data;
+                        // Pre-calculate the parity bit
+                        if (PARITY == "even")
+                            parity_bit <= ^tx_data;
+                        else if (PARITY == "odd")
+                            parity_bit <= ~^tx_data;
+                    end
+                end
+                
+                Start: begin
+                    Uart_tx <= 1'b0;
+                    if (baud_cnt == (Baud_div - 1))
+                        baud_cnt <= 0;
+                    else
+                        baud_cnt <= baud_cnt + 1;
+                end
+                
+                Data: begin
+                    Uart_tx <= shift_reg[0];
+                    if (baud_cnt == (Baud_div - 1)) begin
+                        baud_cnt <= 0;
+                        shift_reg <= {1'b1, shift_reg[Word_len-1:1]}; // Shift in 1s
+                        bit_cnt <= bit_cnt + 1;
+                    end else
+                        baud_cnt <= baud_cnt + 1;
+                end
+
+                Parity: begin
+                    Uart_tx <= parity_bit; // Send the calculated parity bit
+                     if (baud_cnt == (Baud_div - 1))
+                        baud_cnt <= 0;
+                    else
+                        baud_cnt <= baud_cnt + 1;
+                end
+                
+                Stop: begin
+                    Uart_tx <= 1'b1;
+                    if (baud_cnt == (Baud_div - 1))
+                        baud_cnt <= 0;
+                    else
+                        baud_cnt <= baud_cnt + 1;
+                end
+            endcase
+        end
+    end
 endmodule
