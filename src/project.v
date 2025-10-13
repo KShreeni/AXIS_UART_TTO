c87e6cd3449c67f067d80306997997b2c20712b5
KShreeni <109845020+KShreeni@users.noreply.github.com>
2025-10-13 09:39:42 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -18,9 +18,9 @@ module tt_um_top_axis_uart (
 
   // All output pins must be assigned. If not used, assign to 0.
 //  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-    assign uio_out[7:4] = 0;
+    assign uio_out[7:3] = 0;
   assign uio_oe  = 8'b00000011;
-    top_axis_uart #(.DATA_BITS(8)) dut(.clk(clk),.rst(rst_n),.axis_data(ui_in),.axis_valid(uio_in[2]),.axis_last(uio_in[3]),.m_axis_ready(uio_out[2]),.uart_tx(uio_out[0]),.rx_valid(uio_out[1]),.rx_data(uo_out),.parity_err(uio_out[3]));
+    top_axis_uart #(.DATA_BITS(8)) dut(.clk(clk),.rst(rst_n),.axis_data(ui_in),.axis_valid(uio_in[2]),.axis_last(uio_in[3]),.m_axis_ready(uio_out[2]),.uart_tx(uio_out[0]),.rx_valid(uio_out[1]),.rx_data(uo_out));
   // List all unused inputs to prevent warnings
     wire _unused = &{ena, clk, rst_n, uio_in[7:4], uio_in[1:0], 1'b0};
 
