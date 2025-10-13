d4b17040863b561a17a09a59cc5195d6aff5aa81
KShreeni <109845020+KShreeni@users.noreply.github.com>
2025-10-13 12:05:33 +0530

    Update top_axis_uart.v

--- src/top_axis_uart.v
+++ src/top_axis_uart.v
@@ -32,7 +32,7 @@ wire m_axis_valid_out;
 assign m_axis_ready = axis_ready;
 
 axis_master_inp #(.WIDTH(8)) mast_inst(.clk(clk),.rst(rst),.load_data(axis_data),
-.m_axis_valid(axis_valid),.m_axis_ready(axis_ready),.m_axis_last(axis_last),.m_axis_valid_out(m_axis_valid_out),.m_axis_data(m_axis_data));
+.m_axis_valid(axis_valid),.m_axis_ready(axis_ready),.m_axis_valid_out(m_axis_valid_out),.m_axis_data(m_axis_data));
 
 axis_fifo_uart_tx #(.WIDTH(8),.DEPTH(8),.CLK_RATE(50000000),.BAUD(115200)) axis_fifo_uart_tx_inst(.clk(clk),.rst(rst),
 .s_axis_data(m_axis_data),.s_axis_valid(m_axis_valid_out),.s_axis_ready(axis_ready),.s_axis_last(axis_last),.uart_tx(uart_tx));
