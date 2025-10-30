a042458f2e3d3bc136a4c13e4b3e2d236837f693
KShreeni <109845020+KShreeni@users.noreply.github.com>
2025-10-30 20:17:38 +0530

    Update axis_master_inp.v

--- src/axis_master_inp.v
+++ src/axis_master_inp.v
@@ -157,40 +157,28 @@ endmodule*/
 module axis_master_inp #(parameter WIDTH = 8) (
     input  wire clk,
     input  wire rst,
-
-
-
-    
     input  wire [WIDTH-1:0] load_data,
-
     input  wire m_axis_ready,
-    input  wire m_axis_valid,
-   
-     output reg m_axis_valid_out,
-    output reg  [WIDTH-1:0] m_axis_data             // output data
+    input  wire m_axis_valid,   // input from source (testbench)
+    input wire m_axis_last,
+    output reg  m_axis_valid_out,
+    output reg  [WIDTH-1:0] m_axis_data
 );
-   
 
-    
     always @(posedge clk or posedge rst) begin
-        if(rst) begin
-       
+        if (rst) begin
             m_axis_data <= 0;
             m_axis_valid_out <= 0;
+        end else begin
+            // Hold valid high until handshake completes
+          m_axis_valid_out <= m_axis_valid;
+            // note: no "else" resetting valid_out
+
+            // Update data only when handshake occurs
+            if (m_axis_valid && m_axis_ready)
+                m_axis_data <= load_data;
         end
-        else begin
-           if(m_axis_valid && m_axis_ready) begin
-                m_axis_valid_out <= 1;
-                 m_axis_data <= load_data;
-           end
-            else begin
-              m_axis_valid_out <= 0;
-               m_axis_data <=  m_axis_data;
-
-      end
-
     end
-end
 endmodule
 
 
